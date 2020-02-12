3c3dc8fdbee34e5910b60659a7d791a8d8ab73c2
Kevin Wang <kw142143@gmail.com>
2020-02-12 11:32:57 -0500

    nodeJS boilerplate

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const cors = require('cors');
+
+
+
+const app = express();
+
+app.use(cors()) // Use this after the variable declaration
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api', indexRouter);
+
+// Render React page
+app.use(express.static(path.join(__dirname, "../")));
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+module.exports = app;
